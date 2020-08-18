1ef24a2d7a0b4999ac2b60b6325a0e292cab5067
cseibert2667 <cseibert2667@gmail.com>
2020-08-18 14:00:40 -0700

    builds cards and appends to page

--- script.js
+++ script.js
@@ -21,20 +21,22 @@ function getPrices(gameID) {
     allPrices = response.deals;
     for (let i = 0; i < 5; i++) {
       let deals = allPrices[i];
-      console.log(deals);
-      let dealID = deals.dealID;
-      console.log(
-        "LINK TO PAGE -> https://www.cheapshark.com/redirect?dealID=" + dealID
-      );
-      price = deals.price;
-      console.log("PRICE ->" + price);
+      
+      
+      let dealID = deals.dealID; 
+      let dealURL = "https://www.cheapshark.com/redirect?dealID=" + dealID
+      let $link = $("<a>").attr("href", dealURL).text("Buy Now!") // link to retailer
+      
+      let price = deals.price;
+      let $price = $("<p>").addClass("title is-4").text(price) // price
+
       let storeID = deals.storeID;
-      getStoreName(storeID);
+      getStoreName(storeID, $price, $link);
     }
   });
 }
 
-function getStoreName(storeID) {
+function getStoreName(storeID, $price, $link) {
   queryURL = "https://www.cheapshark.com/api/1.0/stores";
   $.ajax({
     url: queryURL,
@@ -42,8 +44,30 @@ function getStoreName(storeID) {
   }).then(function (response) {
     // console.log(response)
     let storeName = response[storeID - 1].storeName;
+    let $retailer = $("<p>").addClass("subtitle is-6").text(storeName)
+    
     let bannerPath = response[storeID - 1].images.banner;
     let bannerURL = "http://www.cheapshark.com" + bannerPath;
+    let $banner = $("<img>").attr("src", bannerURL)
+    
+    let $card = $("<div>").addClass("card"); // creates card
+    let $cardImg = $("<div>").addClass("card-image"); // creates div for card image
+    let $figure = $("<figure>").addClass("image is-3by1"); // img tag gets inserted in here
+    let $cardContent = $("<div>").addClass("card-content")
+    let $media = $("<div>").addClass("media")
+    let $mediaContent = $("<div>").addClass("media-content")
+    let $content = $("<div>").addClass("content")
+    
+    $($content).append($link)
+    $($mediaContent).append($retailer, $price)
+    $($media).append($mediaContent)
+    $($cardContent).append($media, $content)
+    $($figure).append($banner)
+    $($cardImg).append($figure)
+    $($card).append($cardImg, $cardContent)
+    $("body").append($card)
+
+
     console.log(storeName);
     console.log(bannerURL);
   });
