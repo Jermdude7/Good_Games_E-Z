a5e6b30a7f41469e787dba26077f378ddee53f9e
cseibert2667 <cseibert2667@gmail.com>
2020-08-18 12:47:03 -0700

    logs retailer & logo to console

--- script.js
+++ script.js
@@ -1,33 +1,50 @@
-let gameName = "skyrim"
-// Here we are building the URL we need to query the database
-var queryURL = "https://www.cheapshark.com/api/1.0/games?title="+gameName+"&limit=10"
-// Here we run our AJAX call to the OpenWeatherMap API
+let gameName = "skyrim";
+var queryURL =
+  "https://www.cheapshark.com/api/1.0/games?title=" + gameName + "&limit=10";
+
 $.ajax({
   url: queryURL,
-  method: "GET"
-})
-  // We store all of the retrieved data inside of an object called "response"
-  .then(function(response) {
-    console.log(response)
-    let gameID = response[0].gameID // then plug this into a different call for pricing results
-    getPrices(gameID)
-  });
+  method: "GET",
+}).then(function (response) {
+  console.log(response);
+  let gameID = response[0].gameID;
+  getPrices(gameID);
+});
 
-  function getPrices (gameID){
-    queryURL = "https://www.cheapshark.com/api/1.0/games?id="+gameID
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-  }).then(function(response) {
-    console.log(response)
-    allPrices = response.deals
+function getPrices(gameID) {
+  queryURL = "https://www.cheapshark.com/api/1.0/games?id=" + gameID;
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+  }).then(function (response) {
+    console.log(response);
+    allPrices = response.deals;
     for (let i = 0; i < 5; i++) {
       let deals = allPrices[i];
-      console.log(deals)
-      let dealID = deals.dealID
-      console.log("LINK TO PAGE -> https://www.cheapshark.com/redirect?dealID="+dealID)
-      price = deals.price
-      console.log("PRICE ->"+price)
+      console.log(deals);
+      let dealID = deals.dealID;
+      console.log(
+        "LINK TO PAGE -> https://www.cheapshark.com/redirect?dealID=" + dealID
+      );
+      price = deals.price;
+      console.log("PRICE ->" + price);
+      let storeID = deals.storeID;
+      getStoreName(storeID);
     }
-  })
+  });
+}
+
+function getStoreName(storeID) {
+  queryURL = "https://www.cheapshark.com/api/1.0/stores";
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+  }).then(function (response) {
+    // console.log(response)
+    let storeName = response[storeID - 1].storeName;
+    let bannerPath = response[storeID - 1].images.banner;
+    let bannerURL = "http://www.cheapshark.com" + bannerPath;
+    console.log(storeName);
+    console.log(bannerURL);
+  });
 }
