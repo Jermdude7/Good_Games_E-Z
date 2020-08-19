5b0a58b8daecb52fa5c2797fc5ac2cf4c1692bff
cdalt <68070897+cdalt@users.noreply.github.com>
2020-08-19 10:28:07 -0700

    updated rawg (#7)

--- script.js
+++ script.js
@@ -1,3 +1,46 @@
+<<<<<<< HEAD
+let gameInfo = null;
+
+let gameName = "pubg";
+let queryURL = "https://api.rawg.io/api/games?search=";
+let titleHeader = document.getElementById("title");
+let rating = document.getElementById("rating");
+let background = document.getElementById("backgroundImg");
+let platformList = document.getElementById("platforms");
+let released = document.getElementById("release");
+
+function getGame(gameName) {
+  $.ajax({
+    url: queryURL + gameName,
+    method: "GET",
+  }).then(function (res) {
+    response = res.results[0];
+    gameInfo = {
+      title: response.name,
+      rating: response.metacritic,
+      image: response.background_image,
+      platform: response.platforms,
+      released: response.released,
+    };
+    rating.textContent = response.metacritic;
+    titleHeader.textContent = response.name;
+    background.setAttribute("src", response.background_image);
+    //   platformList.textContent =
+    released.textContent = response.released;
+    console.log(response);
+    document.getElementById("gameData").style.display = "block";
+    for (let listElement of response.platforms) {
+      console.log(listElement.platform.name);
+      let platform = document.createElement("li");
+      platform.textContent = listElement.platform.name;
+      platformList.appendChild(platform);
+    }
+  });
+}
+getGame("pubg");
+// create button for evenlistner in order to attach gamefunction TO that button
+// input validation
+=======
 let gameName = "skyrim";
 var queryURL =
   "https://www.cheapshark.com/api/1.0/games?title=" + gameName + "&limit=10";
@@ -72,3 +115,4 @@ function getStoreName(storeID, $price, $link) {
     console.log(bannerURL);
   });
 }
+>>>>>>> c0569a00433a488f9d4f03cadd1c4e162b57fffb
